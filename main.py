c5b680a6357a90c242bd859d088afaf24e35182e
Samuel Blum <53230026+SamuelBlum@users.noreply.github.com>
2019-07-24 01:10:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,23 @@ temp = temp.TempSensor()
 CO2 = CO2.CO2Sensor()
 
 if temp > 100 and CO2 > 100:
-    alarm.trigger()
+        
+    #set phone number and message and trigger car alarm
+    access_key = ''
+    secret_key = ''
+    phone_number = ''
+    message = ''
+
+    # Create SNS client
+    client = boto3.client(
+        "sns",
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        region_name="us-east-1"
+    )
+
+    # Send sms message.
+    client.publish(
+        PhoneNumber=phone_number,
+        Message=message
+    )
